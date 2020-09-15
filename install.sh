e502ead2b783005c55e69f411b2e9e17aea7488b
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-15 21:26:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,10 @@
 apt-get -y update \
     && apt-get -y install wget apt-transport-https software-properties-common gnupg gnupg2 gnupg1
 
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt-get -y update \
+    && apt-get -y install grafana
+
 # chmod scripts
 chmod +x /*.sh
