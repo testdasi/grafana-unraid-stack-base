828ebce339230d666948e7cb3d8ea11d222ff7a7
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 00:39:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@
 apt-get -y update \
     && apt-get -y install wget apt-transport-https software-properties-common gnupg gnupg2 gnupg1 curl unzip jq
 
-# install grafana
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# add grafana repo
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+# add telegraf repo
+wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
+echo "deb https://repos.influxdata.com/debian buster stable" | tee /etc/apt/sources.list.d/influxdb.list
+
+# install grafana telegraf influxdb hddtemp
 apt-get -y update \
-    && apt-get -y install grafana
+    && apt-get -y install grafana telegraf influxdb hddtemp
 
 # install loki
 LOKI_RELEASE=$(curl -sX GET "https://api.github.com/repos/grafana/loki/releases/latest" | jq -r .tag_name)
@@ -25,4 +30,3 @@ LOKI_VER=${LOKI_RELEASE#v} \
     && mv promtail-linux-amd64 /usr/sbin \
     && rm -f promtail-linux-amd64 \
     && echo "$(date "+%d.%m.%Y %T") Added loki and promtail binary release ${LOKI_RELEASE}" >> /build_date.info
-
